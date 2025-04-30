6162e888e3f222a2613af9b3f2f8437dee0496d2
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-30 15:30:03 +0700

    Refactor Product model to include type and semester relationships, and enhance name generation logic

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,20 +12,41 @@ class Product extends Model
     use SoftDeletes, HasUserAuditable;
 
     protected $fillable = [
+        'type_id',
+        'supplier_id',
+        'semester_id',
         'curriculum_id',
         'educational_level_id',
         'educational_class_id',
         'educational_subject_id',
         'code',
+        'name',
         'page_count',
         'price',
         'cost',
         'stock',
     ];
 
+    protected $casts = [
+        'page_count' => 'integer',
+        'price' => 'integer',
+        'cost' => 'integer',
+        'stock' => 'integer',
+    ];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+
     public function supplier(): BelongsTo
     {
         return $this->belongsTo(Supplier::class);
+    }
+
+    public function semester(): BelongsTo
+    {
+        return $this->belongsTo(Semester::class);
     }
 
     public function curriculum(): BelongsTo
@@ -61,14 +82,29 @@ class Product extends Model
         return "{$type}-{$level}{$curriculum}{$subject}{$class}{$semester}/{$supplier}";
     }
 
+    public function getName(): string
+    {
+        $type = $this->type->name;
+        $level = $this->educationalLevel->name;
+        $curriculum = $this->curriculum->name;
+        $subject = $this->educationalSubject->name;
+        $class = $this->educationalClass->name;
+        $semester = $this->semester->name;
+        $supplier = $this->supplier->name;
+
+        return "{$type} - {$level} - {$curriculum} - {$subject} - {$class} - {$semester} - ({$supplier})";
+    }
+
     protected static function booted(): void
     {
         static::creating(function ($model) {
             $model->code = $model->getCode();
+            $model->name = $model->getName();
         });
 
         static::updating(function ($model) {
             $model->code = $model->getCode();
+            $model->name = $model->getName();
         });
     }
 }
