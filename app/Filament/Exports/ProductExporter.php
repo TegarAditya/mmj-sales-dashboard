48c27d8daf28e1edc2c3639d341474b5a248bf5f
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-30 15:30:25 +0700

    Add ProductExporter and ProductImporter classes with column definitions and notification logic

---
+++ app/Filament/Exports/ProductExporter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Exports;
+
+use App\Models\Product;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+
+class ProductExporter extends Exporter
+{
+    protected static ?string $model = Product::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('id')
+                ->label('ID'),
+            ExportColumn::make('name')
+                ->label('Nama'),
+            ExportColumn::make('description')
+                ->label('Deskripsi'),
+            ExportColumn::make('curriculum.code')
+                ->label('Kode Kurikulum'),
+            ExportColumn::make('semester.code')
+                ->label('Kode Semester'),
+            ExportColumn::make('EducationalLevel.code')
+                ->label('Kode Jenjang'),
+            ExportColumn::make('EducationalClass.code')
+                ->label('Kode Kelas'),
+            ExportColumn::make('EducationalSubjects.code')
+                ->label('Kode Mapel'),
+            ExportColumn::make('type.name')
+                ->label('Nama Tipe'),
+            ExportColumn::make('type.code')
+                ->label('Kode Tipe'),
+            ExportColumn::make('sort'),
+            ExportColumn::make('created_at'),
+            ExportColumn::make('updated_at'),
+            ExportColumn::make('deleted_at'),
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your product export has completed and '.number_format($export->successful_rows).' '.str('row')->plural($export->successful_rows).' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' '.number_format($failedRowsCount).' '.str('row')->plural($failedRowsCount).' failed to export.';
+        }
+
+        return $body;
+    }
+}
---
+++ app/Filament/Imports/ProductImporter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Imports;
+
+use App\Models\Product;
+use Filament\Actions\Imports\ImportColumn;
+use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\Models\Import;
+
+class ProductImporter extends Importer
+{
+    protected static ?string $model = Product::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ImportColumn::make('curriculum')
+                ->label('Kode Kurikulum')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+            ImportColumn::make('semester')
+                ->label('Kode Semester')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+            ImportColumn::make('educationalLevel')
+                ->label('Kode Jenjang')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+            ImportColumn::make('educationalClass')
+                ->label('Kode Kelas')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+            ImportColumn::make('educationalSubject')
+                ->label('Kode Mata Pelajaran')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+            ImportColumn::make('type')
+                ->label('Kode Tipe')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+            ImportColumn::make('supplier')
+                ->label('Penerbit')
+                ->requiredMapping()
+                ->relationship(resolveUsing: 'code')
+                ->rules(['required']),
+        ];
+    }
+
+    public function resolveRecord(): ?Product
+    {
+        // return Product::firstOrNew([
+        //     // Update existing records, matching them by `$this->data['column_name']`
+        //     'email' => $this->data['email'],
+        // ]);
+
+        return new Product;
+    }
+
+    public static function getCsvDelimiter(): string
+    {
+        return ';';
+    }
+
+    public static function getCompletedNotificationBody(Import $import): string
+    {
+        $body = 'Your product import has completed and ' . number_format($import->successful_rows) . ' ' . str('row')->plural($import->successful_rows) . ' imported.';
+
+        if ($failedRowsCount = $import->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to import.';
+        }
+
+        return $body;
+    }
+}
