6ada8695edb7c9df7ecf4dfbf368a8cd66e4e904
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-30 15:26:57 +0700

    Add SupplierResource and ManageSuppliers page with form and table definitions

---
+++ app/Filament/Admin/Resources/SupplierResource.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\SupplierResource\Pages;
+use App\Filament\Admin\Resources\SupplierResource\RelationManagers;
+use App\Models\Supplier;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SupplierResource extends Resource
+{
+    protected static ?string $model = Supplier::class;
+
+    protected static ?string $modelLabel = 'Daftar Supplier';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Master Supplier';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('code')
+                    ->label('Kode Supplier')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name')
+                    ->label('Nama')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('address')
+                    ->label('Alamat')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('phone')
+                    ->label('Nomor Telepon')
+                    ->tel()
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('email')
+                    ->label('Alamat Email')
+                    ->email()
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('contact_person_name')
+                    ->label('Nama Contact Person')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('contact_person_phone')
+                    ->label('Nomor Telepon CP')
+                    ->tel()
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('website')
+                    ->label('Website Supplier')
+                    ->columnSpanFull()
+                    ->maxLength(255)
+                    ->default(null),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code')
+                    ->label('Kode Supplier')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->label('Nomor Telepon')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Alamat Email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('website')
+                    ->label('Website')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('contact_person_name')
+                    ->label('Representatif')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('contact_person_phone')
+                    ->label('Nomor Telepon CP')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('createdBy.name')
+                    ->label('Dibuat oleh')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('updatedBy.name')
+                    ->label('Diedit oleh')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deletedBy.name')
+                    ->label('Dihapus oleh')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSuppliers::route('/'),
+        ];
+    }
+}
