ffd00709d7a36111a58b6f5820c069918694e7d9
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-09 14:20:51 +0700

    feat: status badges and improved quantity display

--- app/Filament/Admin/Resources/CustomerResource/Widgets/CustomerEstimationItemTable.php
+++ app/Filament/Admin/Resources/CustomerResource/Widgets/CustomerEstimationItemTable.php
@@ -10,6 +10,7 @@ use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Livewire\Attributes\Url;
 
@@ -50,25 +51,46 @@ class CustomerEstimationItemTable extends BaseWidget
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->label('Nama Produk')
+                    ->wrap()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->label('Status')
+                    ->getStateUsing(fn(Product $record) => $this->getEstimationStatus($record))
+                    ->badge()
+                    ->color(fn($state) => $state === 'Belum Lengkap' ? 'danger' : 'success')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('estimated_quantity')
                     ->label('Jumlah Estimasi')
                     ->sortable()
                     ->numeric()
                     ->default(0)
-                    ->sum(['estimationItems'], 'quantity')
+                    ->alignCenter()
                     ->weight(\Filament\Support\Enums\FontWeight::Bold),
                 Tables\Columns\TextColumn::make('delivered_quantity')
                     ->label('Jumlah Terkirim')
                     ->sortable()
+                    ->numeric()
                     ->default(0)
+                    ->alignCenter()
                     ->weight(\Filament\Support\Enums\FontWeight::Bold),
                 Tables\Columns\TextColumn::make('remaining_quantity')
                     ->label('Sisa Estimasi')
                     ->sortable()
+                    ->numeric()
                     ->default(0)
+                    ->alignCenter()
                     ->weight(\Filament\Support\Enums\FontWeight::Bold)
-                    ->getStateUsing(fn(Product $record) => $record->estimated_quantity - $record->delivered_quantity),
+                    ->getStateUsing(fn(Product $record) => $this->getRemainingQuantity($record)),
             ]);
+    }
+
+    private function getEstimationStatus(Model $record): string
+    {
+        return $record->estimated_quantity > $record->delivered_quantity ? 'Belum Lengkap' : 'Lengkap';
+    }
+
+    private function getRemainingQuantity(Model $record): int
+    {
+        return max(0, $record->estimated_quantity - $record->delivered_quantity);
     }
 }
