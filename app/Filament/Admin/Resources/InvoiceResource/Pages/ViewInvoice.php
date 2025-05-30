1b20a53894e170d73cca626af92b1e444968336b
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-30 13:15:30 +0700

    feat: enhance ViewInvoice page with infolist and footer widgets

--- app/Filament/Admin/Resources/InvoiceResource/Pages/ViewInvoice.php
+++ app/Filament/Admin/Resources/InvoiceResource/Pages/ViewInvoice.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Admin\Resources\InvoiceResource\Pages;
 
 use App\Filament\Admin\Resources\InvoiceResource;
+use App\Filament\Admin\Resources\InvoiceResource\Widgets\InvoiceItemTable;
 use Filament\Actions;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewInvoice extends ViewRecord
@@ -19,6 +22,41 @@ class ViewInvoice extends ViewRecord
                 ->url(route('print.invoice', $this->record->id))
                 ->openUrlInNewTab(),
             Actions\EditAction::make(),
+        ];
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->columns(1)
+            ->schema([
+                Infolists\Components\Section::make()
+                    ->columns(2)
+                    ->schema([
+                        Infolists\Components\TextEntry::make('document_number')
+                            ->label('Kode Invoice')
+                            ->weight('bold'),
+                        Infolists\Components\TextEntry::make('delivery.document_number')
+                            ->label('Surat Jalan'),
+                        Infolists\Components\TextEntry::make('customer.name')
+                            ->label('Nama Pelanggan'),
+                        Infolists\Components\TextEntry::make('semester.name')
+                            ->label('Semester'),
+                        Infolists\Components\TextEntry::make('date')
+                            ->label('Tanggal Invoice')
+                            ->date(),
+                        Infolists\Components\TextEntry::make('total_due')
+                            ->label('Total Tagihan')
+                            ->formatStateUsing(fn($state) => format_currency($state))
+                            ->weight('bold'),
+                    ]),
+            ]);
+    }
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            InvoiceItemTable::class,
         ];
     }
 }
