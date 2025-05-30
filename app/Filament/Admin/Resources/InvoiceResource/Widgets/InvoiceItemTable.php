19bce1a75121d7c5367ac5dd404f7ecb7746ef22
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-30 13:15:22 +0700

    feat: create InvoiceItemTable widget for displaying invoice items

---
+++ app/Filament/Admin/Resources/InvoiceResource/Widgets/InvoiceItemTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Admin\Resources\InvoiceResource\Widgets;
+
+use App\Models\InvoiceItem;
+use App\Models\Product;
+use Filament\Resources\Pages\Concerns\InteractsWithRecord;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class InvoiceItemTable extends BaseWidget
+{
+    use InteractsWithRecord;
+
+    protected int | string | array $columnSpan = 2;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                InvoiceItem::query()
+                    ->where('invoice_id', $this->record->id)
+                    ->with(['product'])
+            )
+            ->heading(null)
+            ->columns([
+                Tables\Columns\TextColumn::make('product.name')
+                    ->label('Nama Produk')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->label('Jumlah')
+                    ->sortable()
+                    ->numeric()
+                    ->formatStateUsing(fn($state) => number_format($state, 0, ',', '.')),
+                Tables\Columns\TextColumn::make('price')
+                    ->label('Harga')
+                    ->sortable()
+                    ->numeric()
+                    ->formatStateUsing(fn($state) => format_currency($state)),
+                Tables\Columns\TextColumn::make('discount')
+                    ->label('Diskon')
+                    ->sortable()
+                    ->numeric()
+                    ->formatStateUsing(fn($state) => format_currency($state)),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->label('Total Harga')
+                    ->sortable()
+                    ->numeric()
+                    ->formatStateUsing(fn($state) => format_currency($state)),
+            ]);
+    }
+}
