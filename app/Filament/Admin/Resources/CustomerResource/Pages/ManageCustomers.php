ad3df843207ad2e652f5a16a6381ea25f0d6eba0
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-09 14:13:40 +0700

    feat: add import and export actions for customer data management

--- app/Filament/Admin/Resources/CustomerResource/Pages/ManageCustomers.php
+++ app/Filament/Admin/Resources/CustomerResource/Pages/ManageCustomers.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Admin\Resources\CustomerResource\Pages;
 
 use App\Filament\Admin\Resources\CustomerResource;
+use App\Filament\Exports\CustomerExporter;
+use App\Filament\Imports\CustomerImporter;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
@@ -13,6 +15,20 @@ class ManageCustomers extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ImportAction::make()
+                ->importer(CustomerImporter::class)
+                ->label('Impor Customer')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->modalHeading('Impor Customer')
+                ->modalDescription('Impor data customer dari file CSV.')
+                ->successNotificationTitle('Customer berhasil diimpor.'),
+            Actions\ExportAction::make()
+                ->exporter(CustomerExporter::class)
+                ->label('Ekspor Customer')
+                ->icon('heroicon-o-arrow-up-tray')
+                ->modalHeading('Ekspor Customer')
+                ->modalDescription('Ekspor data customer ke file CSV.')
+                ->successNotificationTitle('Customer berhasil diekspor.'),
             Actions\CreateAction::make(),
         ];
     }
