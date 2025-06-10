1a4e5eb6f10d53f981cf40b92949797b076af2db
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-10 14:09:55 +0700

    feat: implement create form action with confirmation

--- app/Filament/Admin/Resources/ReturnGoodResource/Pages/CreateReturnGood.php
+++ app/Filament/Admin/Resources/ReturnGoodResource/Pages/CreateReturnGood.php
@@ -4,9 +4,19 @@ namespace App\Filament\Admin\Resources\ReturnGoodResource\Pages;
 
 use App\Filament\Admin\Resources\ReturnGoodResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateReturnGood extends CreateRecord
 {
     protected static string $resource = ReturnGoodResource::class;
+
+    protected function getCreateFormAction(): Action
+    {
+        return Action::make('create')
+            ->label(__('filament-panels::resources/pages/create-record.form.actions.create.label'))
+            ->requiresConfirmation()
+            ->submit('create')
+            ->keyBindings(['mod+s']);
+    }
 }
