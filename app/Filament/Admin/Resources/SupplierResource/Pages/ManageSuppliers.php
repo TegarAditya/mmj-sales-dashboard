8a35c5f8f484796eb93728ac2591a2e07977b6ef
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-10 10:18:16 +0700

    feat: add import and export actions for managing suppliers

--- app/Filament/Admin/Resources/SupplierResource/Pages/ManageSuppliers.php
+++ app/Filament/Admin/Resources/SupplierResource/Pages/ManageSuppliers.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Admin\Resources\SupplierResource\Pages;
 
 use App\Filament\Admin\Resources\SupplierResource;
+use App\Filament\Imports\SupplierImporter;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
@@ -10,9 +11,25 @@ class ManageSuppliers extends ManageRecords
 {
     protected static string $resource = SupplierResource::class;
 
+    protected static ?string $title = 'Daftar Supplier';
+
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ImportAction::make()
+                ->importer(SupplierImporter::class)
+                ->label('Impor Supplier')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->modalHeading('Impor Supplier')
+                ->modalDescription('Impor data Supplier dari file CSV.')
+                ->successNotificationTitle('Supplier berhasil diimpor.'),
+            Actions\ExportAction::make()
+                ->exporter(SupplierImporter::class)
+                ->label('Ekspor Supplier')
+                ->icon('heroicon-o-arrow-up-tray')
+                ->modalHeading('Ekspor Supplier')
+                ->modalDescription('Ekspor data Supplier ke file CSV.')
+                ->successNotificationTitle('Supplier berhasil diekspor.'),
             Actions\CreateAction::make(),
         ];
     }
