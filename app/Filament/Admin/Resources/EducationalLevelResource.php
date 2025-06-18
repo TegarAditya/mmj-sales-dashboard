a6aa16f93dad37646367807aa88e31684863ee37
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-18 14:12:58 +0700

    feat: add infolist to EducationalLevelResource

--- app/Filament/Admin/Resources/EducationalLevelResource.php
+++ app/Filament/Admin/Resources/EducationalLevelResource.php
@@ -8,6 +8,8 @@ use App\Filament\Imports\EducationalLevelImporter;
 use App\Models\EducationalLevel;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -37,6 +39,48 @@ class EducationalLevelResource extends Resource
             ]);
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\Section::make('Data Jenjang')
+                    ->columns(2)
+                    ->icon('heroicon-o-user')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('code')
+                            ->label('Kode Jenjang'),
+                        Infolists\Components\TextEntry::make('name')
+                            ->label('Nama Jenjang'),
+                    ]),
+                Infolists\Components\Section::make('Data Audit')
+                    ->columns(2)
+                    ->icon('heroicon-o-clock')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('createdBy.name')
+                            ->label('Dibuat Oleh'),
+                        Infolists\Components\TextEntry::make('created_at')
+                            ->label('Dibuat Pada')
+                            ->dateTime(),
+                        Infolists\Components\TextEntry::make('updatedBy.name')
+                            ->label('Diedit Oleh')
+                            ->default('-')
+                            ->visible(fn($record) => $record->updatedBy !== null),
+                        Infolists\Components\TextEntry::make('updated_at')
+                            ->label('Diedit Pada')
+                            ->dateTime()
+                            ->visible(fn($record) => $record->updatedBy !== null),
+                        Infolists\Components\TextEntry::make('deletedBy.name')
+                            ->label('Dihapus Oleh')
+                            ->visible(fn($record) => $record->trashed())
+                            ->default('-'),
+                        Infolists\Components\TextEntry::make('deleted_at')
+                            ->label('Dihapus Pada')
+                            ->visible(fn($record) => $record->trashed())
+                            ->dateTime(),
+                    ]),
+            ]);
+    }
+
     public static function table(Table $table): Table
     {
         return $table
@@ -62,6 +106,7 @@ class EducationalLevelResource extends Resource
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
                 Tables\Actions\ForceDeleteAction::make(),
