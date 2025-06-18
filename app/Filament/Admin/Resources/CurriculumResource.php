ba49cc795e757b31301c187f35a43845da60bf29
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-18 14:12:19 +0700

    feat: add infolist to CurriculumResource

--- app/Filament/Admin/Resources/CurriculumResource.php
+++ app/Filament/Admin/Resources/CurriculumResource.php
@@ -8,6 +8,8 @@ use App\Filament\Imports\CurriculumImporter;
 use App\Models\Curriculum;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -17,7 +19,7 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class CurriculumResource extends Resource
 {
     protected static ?string $model = Curriculum::class;
-    
+
     protected static ?string $modelLabel = 'Kurikulum';
 
     protected static ?string $navigationGroup = 'Master Buku';
@@ -34,6 +36,48 @@ class CurriculumResource extends Resource
                     ->maxLength(255),
                 Forms\Components\TextInput::make('name')
                     ->maxLength(255),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\Section::make('Data Kurikulum')
+                    ->columns(2)
+                    ->icon('heroicon-o-user')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('code')
+                            ->label('Kode Kurikulum'),
+                        Infolists\Components\TextEntry::make('name')
+                            ->label('Nama Kurikulum'),
+                    ]),
+                Infolists\Components\Section::make('Data Audit')
+                    ->columns(2)
+                    ->icon('heroicon-o-clock')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('createdBy.name')
+                            ->label('Dibuat Oleh'),
+                        Infolists\Components\TextEntry::make('created_at')
+                            ->label('Dibuat Pada')
+                            ->dateTime(),
+                        Infolists\Components\TextEntry::make('updatedBy.name')
+                            ->label('Diedit Oleh')
+                            ->default('-')
+                            ->visible(fn($record) => $record->updatedBy !== null),
+                        Infolists\Components\TextEntry::make('updated_at')
+                            ->label('Diedit Pada')
+                            ->dateTime()
+                            ->visible(fn($record) => $record->updatedBy !== null),
+                        Infolists\Components\TextEntry::make('deletedBy.name')
+                            ->label('Dihapus Oleh')
+                            ->visible(fn($record) => $record->trashed())
+                            ->default('-'),
+                        Infolists\Components\TextEntry::make('deleted_at')
+                            ->label('Dihapus Pada')
+                            ->visible(fn($record) => $record->trashed())
+                            ->dateTime(),
+                    ]),
             ]);
     }
 
@@ -62,6 +106,7 @@ class CurriculumResource extends Resource
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
                 Tables\Actions\ForceDeleteAction::make(),
